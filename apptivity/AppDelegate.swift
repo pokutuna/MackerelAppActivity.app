cc0b66f0f24d7dfe133043461b03d95093e252aa
pokutuna <popopopopokutuna@gmail.com>
2015-11-14 23:17:02 +0900

    Initial Commit

---
+++ apptivity/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  apptivity
+//
+//  Created by pokutuna on 2015/11/14.
+//  Copyright © 2015年 pokutuna. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
